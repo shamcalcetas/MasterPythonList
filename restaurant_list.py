abcbfaa34cf017ccc04632ef39320d7bb4acc247
shamcalcetas <calcetasshamelle@gmail.com>
2024-10-07 22:24:14 +0800

    docs: adding restaurant_list.py

---
+++ restaurant_list.py
@@ -0,0 +1,9 @@
+restaurant_list = [
+                    {"ID": 1, "Name": "Vikings Luxury Buffet", "Location": "Pasay City", "Cuisine Type": "Buffet", "Established Year": 2011, "Website or Contact": "www.vikings.ph"},
+                    {"ID": 2, "Name": "Antonio's Restaurant", "Location": "Tagaytay", "Cuisine Type": "Fine Dining", "Established Year": 2002, "Website or Contact": "www.antoniosrestaurant.ph"},
+                    {"ID": 3, "Name": "Mesa Filipino Moderne", "Location": "Makati City", "Cuisine Type": "Filipino", "Established Year": 2009, "Website or Contact": "www.mesa.ph"},
+                    {"ID": 4, "Name": "Manam Comfort Filipino", "Location": "Quezon City", "Cuisine Type": "Filipino", "Established Year": 2013, "Website or Contact": "www.manam.ph"},
+                    {"ID": 5, "Name": "Ramen Nagi", "Location": "Various Locations", "Cuisine Type": "Japanese", "Established Year": 2013, "Website or Contact": "www.ramennagi.com.ph"}
+                ]
+
+print(restaurant_list)
