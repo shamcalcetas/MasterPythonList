e153796a3acaf5386a581c7c4f4f2d7d1a695c4b
shamcalcetas <calcetasshamelle@gmail.com>
2024-10-07 22:15:51 +0800

    docs: adding product_list.py

---
+++ product_list.py
@@ -0,0 +1,9 @@
+product_list = [
+                [1, "Laptop", "Electronics", 750, 50, "supplier1@gmail.com"],
+                [2, "Desk Chair", "Furniture", 100, 200, "supplier2@gmail.com"],
+                [3, "Smartwatch", "Electronics", 200, 150, "supplier3@gmail.com"],
+                [4, "Notebook", "Stationery", 5, 500, "supplier4@gmail.com"],
+                [5, "Running Shoes", "Apparel", 80, 100, "supplier5@gmail.com"]
+              ]
+
+print(product_list)
