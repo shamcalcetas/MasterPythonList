74229b1a1d8339352c02a3b1cf29316834c1b7ca
shamcalcetas <calcetasshamelle@gmail.com>
2024-10-07 22:19:11 +0800

    docs: adding employee_list.py

---
+++ employee_list.py
@@ -0,0 +1,9 @@
+employee_list = [
+                  {"ID": 1, "Name": "John Doe", "Department": "Sales", "Age": 30, "Email": "john.doe@company.com"},
+                  {"ID": 2, "Name": "Jane Smith", "Department": "Human Resources", "Age": 25, "Email": "jane.smith@company.com"},
+                  {"ID": 3, "Name": "Mark Johnson", "Department": "IT", "Age": 40, "Email": "mark.johnson@company.com"},
+                  {"ID": 4, "Name": "Lisa Wong", "Department": "Marketing", "Age": 28, "Email": "lisa.wong@company.com"},
+                  {"ID": 5, "Name": "Paul McDonald", "Department": "Finance", "Age": 35, "Email": "paul.mcdonald@company.com"}
+                ]
+
+print(employee_list)
